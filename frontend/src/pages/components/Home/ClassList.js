472e69631913bf0b314371c827b7b5dc91fee316
minhng-178 <anhminh1782002@gmail.com>
2023-05-30 10:18:59 +0700

    Home from home branch

---
+++ frontend/src/pages/components/Home/ClassList.js
@@ -0,0 +1,67 @@
+export const itemData = [
+  {
+    id: "1",
+    img: "/assets/img/1.jpg",
+    title: "BASIC YOGA",
+    place: "In Studio",
+  },
+  {
+    id: "2",
+    img: "/assets/img/2.jpg",
+    title: "HATHA YOGA",
+    place: "In Studio",
+  },
+  {
+    id: "3",
+    img: "/assets/img/3.jpg",
+    title: "VINYASA YOGA",
+    place: "In Studio",
+  },
+  {
+    id: "4",
+    img: "/assets/img/4.jpg",
+    title: "YIN YOGA",
+    place: "In Studio",
+  },
+  {
+    id: "5",
+    img: "/assets/img/5.jpg",
+    title: "KUNDALINI YOGA",
+    place: "In Studio",
+  },
+];
+
+export const itemData2 = [
+  {
+    id: "1",
+    date: "01/04",
+    img: "/assets/img/6.jpg",
+    content:
+      "I like the structure of the classes, the music, and the mix of different exercises. So efficient!",
+    author: "MARI",
+  },
+  {
+    id: "2",
+    date: "02/04",
+    img: "/assets/img/7.jpg",
+    content:
+      "I think you’re all amazing and I now try to arrange my day around Pilates.",
+    author: "ROBERT",
+  },
+  {
+    id: "3",
+    date: "03/04",
+    img: "/assets/img/8.jpg",
+    content:
+      "I did your class. Just like a magic bullet, I felt like someone has sprinkled sunshine on me. I felt positivity, energised, strong, in control of my body and my mind. My limbs felt like they could handle anything, you were making me smile through the screen, as always, you inspired me, I was happy.",
+    author: "SYLVIA",
+  },
+  {
+    id: "4",
+    date: "04/04",
+    img: "/assets/img/9.jpg",
+    content:
+      "Every class gives me energy and makes me happy! And the abs are an extra!",
+    author: "TATIANA",
+  },
+];
