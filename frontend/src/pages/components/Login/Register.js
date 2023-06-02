38e6a556ac7ad243812f7302779ebeb84b1392c8
webbythien <thiengk563@gmail.com>
2023-06-02 11:41:06 +0700

    update

--- frontend/src/pages/components/Login/Register.js
+++ frontend/src/pages/components/Login/Register.js
@@ -39,54 +39,6 @@ function Register() {
 
   return (
     <div className={styles.background_all}>
-<<<<<<< HEAD
-    <div className="container mx-auto">
-      <Toaster position="top-center" reverseOrder={false}></Toaster>
-      <div className="flex justify-center items-center h-screen">
-        <div className={styles.glass}>
-          <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Register</h4>
-            <span className="py-4 text-xl w-2/3 text-center text-grey-500">
-              BECOME A HEARTBEAT MEMBER
-            </span>
-          </div>
-          <form className="py-1" onSubmit={formik.handleSubmit}>
-            <div className="textbox flex flex-col items-center gap-6">
-              <input
-                {...formik.getFieldProps("email")}
-                className={styles.textbox}
-                type="email"
-                placeholder="Your Email*"
-              />
-              <input
-                {...formik.getFieldProps("username")}
-                className={styles.textbox}
-                type="text"
-                placeholder="What's Your Fullname*"
-              />
-              <input
-                {...formik.getFieldProps("password")}
-                className={styles.textbox}
-                type="password"
-                placeholder="Your Password*"
-              />
-              <input
-                {...formik.getFieldProps("phone")}
-                className={styles.textbox}
-                type="text"
-                placeholder="Your Phone Number"
-              />
-              <button className={styles.btn} type="submit">
-                Register
-              </button>
-            </div>
-            <div className="text-center py-4">
-              <span className="text-gray-500">
-                Already Register?
-                <Link className="text-red-500" to="/login">
-                    Login Now
-                </Link>
-=======
       <div className="container mx-auto">
         <Toaster position="top-center" reverseOrder={false}></Toaster>
         <div className="flex justify-center items-center h-screen">
@@ -95,7 +47,6 @@ function Register() {
               <h4 className="text-5xl font-bold">Register</h4>
               <span className="py-4 text-xl w-2/3 text-center text-grey-500">
                 BECOME A HEARTBEAT MEMBER
->>>>>>> 1a5f4556c7cf152a67e592b7345756ced40aa007
               </span>
             </div>
             <form className="py-1" onSubmit={formik.handleSubmit}>
--- frontend/src/pages/components/Profile/Profile.js
+++ frontend/src/pages/components/Profile/Profile.js
@@ -142,67 +142,6 @@ function Profile() {
   const imgStyle = `${styles.profile_img} object-cover h-44  `;
   return (
     <div className={styles.background_all}>
-<<<<<<< HEAD
-    <div className="container mx-auto">
-      <Toaster position="top-center" reverseOrder={false}></Toaster>
-      <div className="flex justify-center items-center ">
-        <div className={styles.glass}>
-          <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Profile</h4>
-            <span className="py-4 text-xl w-2/3 text-center text-grey-500">
-              You can update the details
-            </span>
-          </div>
-          <form className="py-1" onSubmit={formik.handleSubmit}>
-            <div style={{width:150,height:"auto",margin:"auto"}} className="profile flex justify-center py-4">
-              <label htmlFor="profile">
-                <img 
-                  src={imageTemp || user.avatar || avatar}
-                  className={imgStyle}
-                  alt="avatar"
-                  onError={loadImageAgain}
-                />
-              </label>
-              <input
-                onChange={onUpload}
-                type="file"
-                id="profile"
-                name="avatar"
-                style={{ width: 500, height: 500 }}
-              />
-            </div>
-
-            <div className="textbox flex flex-col items-center gap-6">
-              <input
-                {...formik.getFieldProps("email")}
-                readOnly
-                className={styles.textbox}
-                type="email"
-                placeholder="Email*"
-              />
-              <input
-                {...formik.getFieldProps("username")}
-                className={styles.textbox}
-                type="text"
-                placeholder="Username*"
-              />
-              <input
-                {...formik.getFieldProps("phone")}
-                className={styles.textbox}
-                type="text"
-                placeholder="phone"
-              />
-              <button className={styles.btn} type="submit">
-                Update
-              </button>
-            </div>
-            <div className="text-center py-4">
-              <span className="text-gray-500">
-                Come back later
-                <button onClick={handleLogout} className="text-red-500" to="/">
-                  Logout Now
-                </button>
-=======
       <div className="container mx-auto">
         <Toaster position="top-center" reverseOrder={false}></Toaster>
         <div className="flex justify-center items-center">
@@ -211,7 +150,6 @@ function Profile() {
               <h4 className="text-5xl font-bold">Profile</h4>
               <span className="py-4 text-xl w-2/3 text-center text-grey-500">
                 You can update the details
->>>>>>> 1a5f4556c7cf152a67e592b7345756ced40aa007
               </span>
             </div>
             <form className="py-1" onSubmit={formik.handleSubmit}>
