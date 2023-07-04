0558bfc99ad2ad9c3b962e5571e73986181ccb56
Duong Minh Tan <dmtan1510@gmail.com>
2023-07-05 01:56:42 +0700

    tan commit

--- frontend/src/pages/components/NotificationNew/News.js
+++ frontend/src/pages/components/NotificationNew/News.js
@@ -1,67 +1,116 @@
-import React, { useEffect, useState } from 'react';
-import { Typography, List, ListItem, ListItemText, Paper } from '@mui/material';
-
-const articles = [
-  {
-    id: 1,
-    title: 'Article 1',
-    content: 'Content of Article 1',
-  },
-  {
-    id: 2,
-    title: 'Article 2',
-    content: 'Content of Article 2',
-  },
-  {
-    id: 3,
-    title: 'Article 3',
-    content: 'Content of Article 3',
-  },
-];
-
-
-
-
-
+import React, { useEffect, useState } from "react";
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Paper,
+  Modal,
+  Button,
+} from "@mui/material";
+import axios from "axios";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const NewsPage = () => {
   const [selectedArticle, setSelectedArticle] = useState(null);
 
   const handleArticleClick = (id) => {
-    const article = articles.find((article) => article.id === id);
-    setSelectedArticle(article);
+    const listNew = listNews.find((article) => article._id === id);
+    setSelectedArticle(listNew);
+  };
+
+  const [listNews, setListNews] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    function compare(a, b) {
+      if (a.createdAt > b.createdAt) {
+        return -1;
+      }
+      if (a.createdAt < b.createdAt) {
+        return 1;
+      }
+      return 0;
+    }
+    async function fetchNews() {
+      try {
+        const response = await axios.get("http://localhost:3001/api/news/get");
+       const statuses = response.data.filter((x) => x.status === true)
+        setListNews(statuses.sort(compare));
+      } catch (error) {
+        console.log("Failed to fetch news");
+      }
+    }
+    fetchNews();
+  }, []);
+
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setOpenModal(false);
   };
 
   return (
     <div>
-      <Typography variant="h4" gutterBottom>
-        News Page
-      </Typography>
+      <Header/>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", fontFamily:'cursive' }}> <Typography variant="h4" gutterBottom >
+       Notification
+      </Typography></div>
+     
 
-      <Paper style={{ maxHeight: '400px', overflow: 'auto' }}>
-        <List component="nav">
-          {articles.map((article) => (
-            <ListItem
-              key={article.id}
-              button
-              onClick={() => handleArticleClick(article.id)}
+      <Paper style={{ maxHeight: '400px', overflow: 'auto', marginBottom: '20px' }}>
+      <List component="nav">
+        {listNews.map((article) => (
+          <ListItem
+            key={article._id}
+            button
+            onClick={() => handleArticleClick(article._id)}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              padding: '10px',
+              cursor: 'pointer',
+              transition: 'background-color 0.3s ease'
+            }}
+          >
+          
+            <ListItemText style={{ marginLeft: '10px' }} primary={article.subject} />
+            <Button
+              onClick={handleModalOpen}
+              style={{
+                backgroundColor: 'transparent',
+                border: 'none',
+                outline: 'none',
+                cursor: 'pointer',
+                marginLeft: 'auto'
+              }}
             >
-              <ListItemText primary={article.title} />
-            </ListItem>
-          ))}
-        </List>
-      </Paper>
+              see more
+            </Button>
+          </ListItem>
+        ))}
+      </List>
+    </Paper>
 
       {selectedArticle && (
         <div>
-          <Typography variant="h6" gutterBottom>
-            {selectedArticle.title}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            {selectedArticle.content}
-          </Typography>
+          <Modal open={openModal} onClose={handleModalClose}>
+            <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: "60%", backgroundColor: "#fff", padding: "20px" }}>
+              <Typography variant="h6" gutterBottom>
+                {selectedArticle.subject}
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                {selectedArticle.content}
+              </Typography>
+            </div>
+          </Modal>
+         
         </div>
       )}
+      <Footer/>
     </div>
   );
 };
