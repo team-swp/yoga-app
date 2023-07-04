2862b49355f8de6e1e5bb8a7aaad9fb93704fde1
minhng-178 <anhminh1782002@gmail.com>
2023-07-04 16:08:07 +0700

    thu 3

--- frontend/src/pages/components/StaffManage/ManageMember/ModalConfirm.js
+++ frontend/src/pages/components/StaffManage/ManageMember/ModalConfirm.js
@@ -1,8 +1,15 @@
-import { Box, Button, Modal, Typography } from "@mui/material";
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
 import { getPayment, updatePayment } from "../../../../helper/paymentAPI";
 import { getMember, updateUserForStaff } from "../../../../helper/loginAPI";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+
+const moment = require("moment");
 
 const style = {
   position: "absolute",
@@ -220,6 +227,208 @@ export function ModalConfirmMember({
               Cancel
             </Button>
           </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export function ModalAddDayOff({ open, handleClose, metaData }) {
+  // const [metaData, setMetaData] = useState(null);
+  const [isValue, setIsValue] = useState(null);
+  const [dayName, setDayName] = useState();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  const adapter = new AdapterDayjs();
+  const currentDate = adapter.date(new Date());
+
+  // Hàm sinh ra khoảng ngày
+  function generateDateRange(startDate, endDate) {
+    const dateRange = [];
+    let currentDate = new Date(startDate);
+    currentDate.setHours(0, 0, 0, 0);
+
+    while (currentDate <= endDate) {
+      dateRange.push(new Date(currentDate));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dateRange;
+  }
+
+  const dateRangeArray = [];
+
+  metaData.forEach((member) => {
+    const memberId = member._id;
+    const meta_data = JSON.parse(member.meta_data);
+    const startDateMember = moment(meta_data.startDateMember).format(
+      "MM/DD/YYYY"
+    );
+
+    const MemberDuration = meta_data.MemberDuration;
+    let expiredDate;
+
+    if (meta_data.isTry) {
+      const durationArray = MemberDuration.split(" ");
+      const durationValue = parseInt(durationArray[0]);
+
+      expiredDate = moment(startDateMember)
+        .add(durationValue, "days")
+        .format("MM/DD/YYYY");
+    } else {
+      expiredDate = moment(startDateMember)
+        .add(MemberDuration, "months")
+        .format("MM/DD/YYYY");
+    }
+
+    const formatedStartDate = new Date(startDateMember);
+    const formatedExpiredDate = new Date(expiredDate);
+
+    const dateRange = generateDateRange(formatedStartDate, formatedExpiredDate);
+    dateRange.memberId = memberId;
+    dateRangeArray.push(dateRange);
+  });
+
+  const dayOffArray = [];
+  const formatStartDate = new Date(startDate);
+  const formatEndDate = new Date(endDate);
+
+  const dateRange = generateDateRange(formatStartDate, formatEndDate);
+
+  dayOffArray.push(dateRange);
+  const resultArray = [];
+
+  dateRangeArray.forEach((dateRange) => {
+    const memberId = dateRange.memberId;
+    const isMemberOnLeave = dateRange.some((date) =>
+      dayOffArray[0].some((offDate) => offDate.getTime() === date.getTime())
+    );
+
+    if (isMemberOnLeave) {
+      resultArray.push(memberId);
+    }
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+  };
+
+  const resetForm = () => {
+    setStartDate(null);
+    setEndDate(null);
+  };
+
+  const closeModal = () => {
+    resetForm();
+    handleClose();
+  };
+
+  const formik = useFormik({
+    initialValues: {},
+    validateOnBlur: false,
+    validateOnChange: false,
+
+    onSubmit: async (values) => {
+      if (startDate && endDate) {
+        if (new Date(endDate) < new Date(startDate)) {
+          toast.error("The end date must be greater the start date");
+          return;
+        }
+
+        if (new Date(startDate).getTime() === new Date(endDate).getTime()) {
+          toast.error("The end date must no equal with the start date");
+          return;
+        }
+      }
+
+      const timeDiff =
+        new Date(endDate).getTime() - new Date(startDate).getTime();
+      const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    },
+  });
+
+  const handleChange = (e) => {
+    setIsValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <Toaster></Toaster>
+      <Modal
+        open={open}
+        onClose={closeModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add new day off
+          </Typography>
+          <form onChange={handleChange} onSubmit={formik.handleSubmit}>
+            <TextField
+              {...formik.getFieldProps("Day off name")}
+              label="Day off name"
+              type="text"
+              fullWidth
+              required
+              style={{ marginTop: "1em", width: "245px" }}
+              error={
+                formik.touched["Day off name"] && formik.errors["Day off name"]
+              }
+              helperText={
+                formik.touched["Day off name"] && formik.errors["Day off name"]
+              }
+            />
+            <div style={{ marginTop: "1em" }}>
+              <LocalizationProvider dateAdapter={AdapterDayjs} locale="en">
+                <DatePicker
+                  label="Start Date"
+                  inputFormat="MM/DD/YYYY"
+                  animateYearScrolling
+                  fullWidth
+                  required
+                  minDate={currentDate}
+                  value={startDate}
+                  onChange={(newValue) =>
+                    setStartDate(dayjs(newValue).format("MM/DD/YYYY"))
+                  }
+                />
+              </LocalizationProvider>
+            </div>
+            <div style={{ marginTop: "1em" }}>
+              <LocalizationProvider dateAdapter={AdapterDayjs} locale="en">
+                <DatePicker
+                  label="End Date"
+                  inputFormat="MM/DD/YYYY"
+                  animateYearScrolling
+                  fullWidth
+                  required
+                  minDate={currentDate}
+                  value={endDate}
+                  onChange={(newValue) =>
+                    setEndDate(dayjs(newValue).format("MM/DD/YYYY"))
+                  }
+                />
+              </LocalizationProvider>
+            </div>
+
+            <Box
+              sx={{
+                mt: 2,
+                display: "flex",
+                justifyContent: "space-between",
+                alignContent: "center",
+              }}
+            >
+              <Button color="success" variant="contained" type="submit">
+                Add
+              </Button>
+              <Button variant="text" onClick={closeModal}>
+                Cancel
+              </Button>
+            </Box>
+          </form>
         </Box>
       </Modal>
     </div>
