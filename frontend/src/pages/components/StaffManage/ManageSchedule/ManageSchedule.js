2ba49a620ed970b8a0b6a251df66715cef19db22
minhng-178 <anhminh1782002@gmail.com>
2023-07-06 11:37:46 +0700

    7/6 11:37

--- frontend/src/pages/components/StaffManage/ManageSchedule/ManageSchedule.js
+++ frontend/src/pages/components/StaffManage/ManageSchedule/ManageSchedule.js
@@ -20,23 +20,19 @@ import { Toaster, toast } from "react-hot-toast";
 import { updateSchedule } from "../../../../helper/scheduleAPI";
 import StatusButton from "./StatusButons";
 import axios from "axios";
-import RestartAltOutlinedIcon from '@mui/icons-material/RestartAltOutlined';
+import RestartAltOutlinedIcon from "@mui/icons-material/RestartAltOutlined";
 import { getClass, updateClass } from "../../../../helper/classAPI";
 
-
-
 function ManageSchedule() {
-  const [classs, setClasss] = useState([])
-  const [scheduleList, setScheduleList] = useState()
+  const [classs, setClasss] = useState([]);
+  const [scheduleList, setScheduleList] = useState();
   const [schedules, setSchedules] = useState([]);
-  const [manageEditSchedule, setManageEditSchedule] = useState({})
-  const [page, setPage] = useState(1)
-  const [pageCount, setPageCount] = useState(0)
-  const [searchQuery, setSearchQuery] = useState('');
+  const [manageEditSchedule, setManageEditSchedule] = useState({});
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
+  const [searchQuery, setSearchQuery] = useState("");
   const [confirmModalOpen, setConfirmModalOpen] = useState(false);
   const [selectedSchedule, setSelectedSchedule] = useState(null);
-
-
 
   const handleToggle = async (event, schedule) => {
     setSelectedSchedule(schedule);
@@ -44,7 +40,10 @@ function ManageSchedule() {
   };
   const handleConfirm = async () => {
     try {
-      const updatedScheduleData = { ...selectedSchedule, status: !selectedSchedule.status };
+      const updatedScheduleData = {
+        ...selectedSchedule,
+        status: !selectedSchedule.status,
+      };
       const scheduleResponse = await updateSchedule(updatedScheduleData);
       if (scheduleResponse && scheduleResponse.data) {
         console.log(scheduleResponse.data.data.schedulename);
@@ -52,8 +51,8 @@ function ManageSchedule() {
         const classResponse = await getClass();
         const classData = classResponse.data;
         if (Array.isArray(classData) && classData.length > 0) {
-          const classWithSchedule = classData.filter((classes) =>
-            classes.schedule_id === scheduleResponse.data.data._id
+          const classWithSchedule = classData.filter(
+            (classes) => classes.schedule_id === scheduleResponse.data.data._id
           );
           if (classWithSchedule.length > 0) {
             classWithSchedule.forEach(async (classes) => {
@@ -66,7 +65,9 @@ function ManageSchedule() {
                 if (response && response.data) {
                   console.log(response.data.data.classname);
                   const updatedClasses = classs.map((classItem) =>
-                    classItem._id === response.data._id ? response.data : classData
+                    classItem._id === response.data._id
+                      ? response.data
+                      : classData
                   );
                   setClasss(updatedClasses);
                 }
@@ -75,13 +76,15 @@ function ManageSchedule() {
               }
             });
           } else {
-            console.log('No class found with the updated schedule');
+            console.log("No class found with the updated schedule");
           }
         } else {
-          console.log('Class data is emty or invalid');
+          console.log("Class data is emty or invalid");
         }
-        toast.success(`${scheduleResponse.data.data.schedulename} status updated succesfully`);
-        setManageEditSchedule(schedules)
+        toast.success(
+          `${scheduleResponse.data.data.schedulename} status updated succesfully`
+        );
+        setManageEditSchedule(schedules);
         setSchedules([...schedules]);
         setConfirmModalOpen(false);
       }
@@ -99,18 +102,22 @@ function ManageSchedule() {
   }, [manageEditSchedule, page]);
 
   async function fetchSchedule2() {
-    const response = await axios.get(`http://localhost:3001/api/schedulesPaging/get?page=${page}&limit=${100}&q=${searchQuery}`);
+    const response = await axios.get(
+      `http://localhost:3001/api/schedulesPaging/get?page=${page}&limit=${100}&q=${searchQuery}`
+    );
     const schedulesData = response.data.items;
-    setPage(response.data.pagination.pageNum)
-    setPageCount(response.data.pagination.pageCount)
+    setPage(response.data.pagination.pageNum);
+    setPageCount(response.data.pagination.pageCount);
     setSchedules(schedulesData);
   }
 
   async function fetchSchedules() {
-    const response = await axios.get(`http://localhost:3001/api/schedulesPaging/get?page=${page}&limit=${3}`);
+    const response = await axios.get(
+      `http://localhost:3001/api/schedulesPaging/get?page=${page}&limit=${3}`
+    );
     const schedulesData = response.data.items;
-    setPage(response.data.pagination.pageNum)
-    setPageCount(response.data.pagination.pageCount)
+    setPage(response.data.pagination.pageNum);
+    setPageCount(response.data.pagination.pageCount);
     setSchedules(schedulesData);
   }
 
@@ -130,11 +137,11 @@ function ManageSchedule() {
 
   const handleSearch = () => {
     fetchSchedule2();
-  }
+  };
 
   const handleReset = () => {
     fetchSchedules();
-    setSearchQuery('');
+    setSearchQuery("");
   };
 
   function handlePrevious() {
@@ -151,7 +158,6 @@ function ManageSchedule() {
     });
   }
 
-
   return (
     <div>
       <Container>
@@ -159,7 +165,12 @@ function ManageSchedule() {
 
         <TableContainer component={Paper}>
           <div
-            style={{ float: "right", marginTop: "15px", marginRight: "10px", marginBottom: '1em' }}
+            style={{
+              float: "right",
+              marginTop: "15px",
+              marginRight: "10px",
+              marginBottom: "1em",
+            }}
           >
             <Button
               variant="contained"
@@ -170,35 +181,50 @@ function ManageSchedule() {
               Add new Schedule
             </Button>
           </div>
-          <div style={{
-            margin: "auto",
-            padding: "15px",
-            maxWidth: "800px",
-            display: "flex",
-            alignItems: "center",
-          }}>
+          <div
+            style={{
+              margin: "auto",
+              padding: "15px",
+              maxWidth: "800px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
             <TextField
               label="Search"
               value={searchQuery}
-              onChange={(event) => setSearchQuery(event.target.value.toLowerCase())}
+              onChange={(event) =>
+                setSearchQuery(event.target.value.toLowerCase())
+              }
               style={{ marginRight: "1rem" }}
             />
             <IconButton onClick={handleReset} sx={{ ml: -8 }}>
               <RestartAltOutlinedIcon />
             </IconButton>
-            <Button onClick={handleSearch} type="submit" variant="contained" color="primary" sx={{ ml: 3 }}>
+            <Button
+              onClick={handleSearch}
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ ml: 3 }}
+            >
               Search
             </Button>
-
           </div>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell style={{ textAlign: "center" }}>Schedule Name</TableCell>
-                <TableCell style={{ textAlign: "center" }}>Start Time</TableCell>
+                <TableCell style={{ textAlign: "center" }}>
+                  Schedule Name
+                </TableCell>
+                <TableCell style={{ textAlign: "center" }}>
+                  Start Time
+                </TableCell>
                 <TableCell style={{ textAlign: "center" }}>End Time</TableCell>
                 <TableCell style={{ textAlign: "center" }}>Status</TableCell>
-                <TableCell style={{ textAlign: "center" }}>Disable-Enable</TableCell>
+                <TableCell style={{ textAlign: "center" }}>
+                  Disable-Enable
+                </TableCell>
                 <TableCell style={{ textAlign: "center" }}>Action</TableCell>
               </TableRow>
             </TableHead>
@@ -218,7 +244,7 @@ function ManageSchedule() {
                     <Switch
                       checked={scheduleItem.status}
                       onChange={(event) => handleToggle(event, scheduleItem)}
-                      color={scheduleItem.status ? 'success' : 'error'}
+                      color={scheduleItem.status ? "success" : "error"}
                     />
                   </TableCell>
                   <TableCell style={{ textAlign: "center" }}>
@@ -230,7 +256,7 @@ function ManageSchedule() {
                       to={`/updateschedule/${scheduleItem._id}`}
                       variant="contained"
                       color="secondary"
-                      style={{ fontSize: "10px", backgroundColor: 'orange' }}
+                      style={{ fontSize: "10px", backgroundColor: "orange" }}
                     >
                       Update
                     </Button>
@@ -246,17 +272,60 @@ function ManageSchedule() {
           closeAfterTransition
         >
           <Fade in={confirmModalOpen}>
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
-              <Paper style={{ width: "400px", padding: "2em", backgroundColor: "#fff", borderRadius: "8px", boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)", textAlign: "center" }} elevation={3}>
-                <h3 style={{ marginBottom: "1em", fontSize: "1.5em", fontWeight: "bold" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <Paper
+                style={{
+                  width: "400px",
+                  padding: "2em",
+                  backgroundColor: "#fff",
+                  borderRadius: "8px",
+                  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+                  textAlign: "center",
+                }}
+                elevation={3}
+              >
+                <h3
+                  style={{
+                    marginBottom: "1em",
+                    fontSize: "1.5em",
+                    fontWeight: "bold",
+                  }}
+                >
                   Confirmation
                 </h3>
-                <p>Are you sure you want to change the status of this Schedule?</p>
-                <div style={{ display: "flex", justifyContent: "flex-end", marginTop: "2rem" }}>
-                  <Button variant="contained" onClick={handleConfirm} style={{ marginRight: "1rem", backgroundColor: "black" }}>
+                <p>
+                  Are you sure you want to change the status of this Schedule?
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    marginTop: "2rem",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    onClick={handleConfirm}
+                    style={{ marginRight: "1rem", backgroundColor: "black" }}
+                  >
                     Confirm
                   </Button>
-                  <Button variant="outlined" onClick={handleCancel} style={{ backgroundColor: "#fff", color: "#000", border: "2px solid #000" }}>
+                  <Button
+                    variant="outlined"
+                    onClick={handleCancel}
+                    style={{
+                      backgroundColor: "#fff",
+                      color: "#000",
+                      border: "2px solid #000",
+                    }}
+                  >
                     Cancel
                   </Button>
                 </div>
--- frontend/src/pages/components/StaffManage/MangeSemester/ManageUpdateSemester.js
+++ frontend/src/pages/components/StaffManage/MangeSemester/ManageUpdateSemester.js
@@ -1,152 +1,173 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getSemester, updateSemester } from "../../../../helper/semesterAPI";
-import {
-    Container,
-    TextField,
-    Button,
-
-} from "@mui/material";
+import { Container, TextField, Button } from "@mui/material";
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
 import { Toaster, toast } from "react-hot-toast";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useNavigate } from "react-router-dom";
 
 function ManageUpdateSemester() {
-    const [semester, setSemester] = useState({});
-    const semesterId = useParams();
-    const [semestername, setSemestername] = useState("");
-    const [startDate, setStartDate] = useState("");
-    const [endDate, setEndDate] = useState("");
-    const [status, setStatus] = useState("");
-    const navigate = useNavigate()
+  const [semester, setSemester] = useState({});
+  const semesterId = useParams();
+  const [semestername, setSemestername] = useState("");
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState("");
+  const [status, setStatus] = useState("");
+  const navigate = useNavigate();
 
-    const handleBack = () => {
-        navigate("/staffmanage")
+  const handleBack = () => {
+    navigate("/staffmanage");
+  };
+
+  useEffect(() => {
+    fetchSemester();
+  }, [semesterId]);
+
+  const apdapter = new AdapterDayjs();
+
+  async function fetchSemester() {
+    try {
+      const response = await getSemester();
+      const semester = response.data.find((obj) => obj._id === semesterId.id);
+      console.log(semester);
+      setSemester(semester);
+
+      const adapStartDate = apdapter.date(new Date(semester.startDate));
+      const adapEndDate = apdapter.date(new Date(semester.endDate));
+
+      // Set initial values for the input fields
+      setSemestername(semester.semestername);
+
+      setStartDate(adapStartDate);
+      setEndDate(adapEndDate);
+      setStatus(semester.status);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(() => {
-        fetchSemester();
-    }, [semesterId]);
-
-    async function fetchSemester() {
-        try {
-            const response = await getSemester();
-            const semester = response.data.find((obj) => obj._id === semesterId.id);
-
-
-            setSemester(semester);
-            // Set initial values for the input fields
-            setSemestername(semester.semestername);
-            // setStartDate(semester.startDate);
-            // setEndDate(semester.endDate);
-            setStatus(semester.status);
-        } catch (error) {
-            console.error(error);
-        }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await updateSemester({
+        _id: semesterId.id,
+        semestername: semestername,
+        startDate: startDate,
+        endDate: endDate,
+        status: status,
+      });
+      if (response) {
+        toast.success(" Updated successfully!");
+      } else {
+        toast.error("Failed to update...");
+      }
+    } catch (error) {
+      toast.error("Failed to update...", error);
     }
+  }
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        try {
-            const response = await updateSemester({
-                _id: semesterId.id,
-                semestername: semestername,
-                startDate: startDate,
-                endDate: endDate,
-                status: status,
-            });
-            if (response) {
-                toast.success(" Updated successfully!");
-            } else {
-                toast.error("Failed to update...");
-            }
-        } catch (error) {
-            toast.error("Failed to update...", error);
-        }
-    }
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+  };
 
-    const handleStartDateChange = (date) => {
-        setStartDate(date);
-    };
+  const handleEndtDateChange = (date) => {
+    setEndDate(date);
+  };
 
-    const handleEndtDateChange = (date) => {
-        setEndDate(date);
-    };
-
-
-    return (
-        <>
-            <Header />
-            <Container>
-                <Toaster position="top-center"></Toaster>
-                <div style={{ maxWidth: '400px', margin: '0 auto' }}>
-                    <h1 style={{ textAlign: 'center', color: '#333', fontSize: '24px', marginBottom: '20px', marginTop: '1em' }}>Update Semester</h1>
-                </div>
-                <form onSubmit={handleSubmit} style={{ marginLeft: '39%' }}>
-                    <TextField
-                        label="Semester Name"
-                        type="text"
-                        name="semester"
-                        value={semestername}
-                        onChange={(event) => setSemestername(event.target.value)}
-                        required
-                        style={{ width: 250 }}
-                    />
-                    <div style={{ marginTop: '1em' }}>
-                        <LocalizationProvider dateAdapter={AdapterDayjs} locale="en">
-                            <DatePicker
-                                label="Start Date"
-                                value={startDate}
-                                onChange={handleStartDateChange}
-                                inputFormat="MM/DD/YYYY"
-                                animateYearScrolling
-                                fullWidth
-                                required
-                            />
-                        </LocalizationProvider>
-                    </div>
-                    <div style={{ marginTop: '1em' }}>
-                        <LocalizationProvider dateAdapter={AdapterDayjs} locale="en" >
-                            <DatePicker
-                                label="End Date"
-                                value={endDate}
-                                onChange={handleEndtDateChange}
-                                inputFormat="MM/DD/YYYY"
-                                animateYearScrolling
-                                fullWidth
-                                required
-                            />
-                        </LocalizationProvider>
-                    </div>
-                    <Button type="submit" variant="contained" style={{ backgroundColor: '#007bff', color: '#fff', fontWeight: 'bold', cursor: 'pointer', marginBottom: '1em', marginTop: '1em' }}>
-                        Update Semester
-                    </Button>
-                    <Button
-                        onClick={handleBack}
-                        style={{
-                            marginBlock: "20px",
-                            float: "right",
-                            backgroundColor: "grey",
-                            border: "none",
-                            color: "white",
-                            padding: "10px 20px",
-                            textAlign: "center",
-                            textDecoration: "none",
-                            display: "inline-block",
-                            fontSize: "10px",
-                            cursor: "pointer",
-                        }}
-                    >
-                        Back
-                    </Button>
-                </form>
-            </Container>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Container>
+        <Toaster position="top-center"></Toaster>
+        <div style={{ maxWidth: "400px", margin: "0 auto" }}>
+          <h1
+            style={{
+              textAlign: "center",
+              color: "#333",
+              fontSize: "24px",
+              marginBottom: "20px",
+              marginTop: "1em",
+            }}
+          >
+            Update Semester
+          </h1>
+        </div>
+        <form onSubmit={handleSubmit} style={{ marginLeft: "39%" }}>
+          <TextField
+            label="Semester Name"
+            type="text"
+            name="semester"
+            value={semestername}
+            onChange={(event) => setSemestername(event.target.value)}
+            required
+            style={{ width: 250 }}
+          />
+          <div style={{ marginTop: "1em" }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} locale="en">
+              <DatePicker
+                label="Start Date"
+                value={startDate}
+                onChange={handleStartDateChange}
+                inputFormat="MM/DD/YYYY"
+                animateYearScrolling
+                fullWidth
+                required
+              />
+            </LocalizationProvider>
+          </div>
+          <div style={{ marginTop: "1em" }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} locale="en">
+              <DatePicker
+                label="End Date"
+                value={endDate}
+                onChange={handleEndtDateChange}
+                inputFormat="MM/DD/YYYY"
+                animateYearScrolling
+                fullWidth
+                required
+              />
+            </LocalizationProvider>
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{
+              backgroundColor: "#007bff",
+              color: "#fff",
+              fontWeight: "bold",
+              cursor: "pointer",
+              marginBottom: "1em",
+              marginTop: "1em",
+            }}
+          >
+            Update Semester
+          </Button>
+          <Button
+            onClick={handleBack}
+            style={{
+              marginBlock: "20px",
+              float: "right",
+              backgroundColor: "grey",
+              border: "none",
+              color: "white",
+              padding: "10px 20px",
+              textAlign: "center",
+              textDecoration: "none",
+              display: "inline-block",
+              fontSize: "10px",
+              cursor: "pointer",
+            }}
+          >
+            Back
+          </Button>
+        </form>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 export default ManageUpdateSemester;
