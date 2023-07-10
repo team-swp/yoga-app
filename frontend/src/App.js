04bb70d7905b73cf21e0d343886fc9f7fb3a0a39
minhng-178 <anhminh1782002@gmail.com>
2023-07-10 14:39:38 +0700

    2:43

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,31 +10,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUserLogin, setDataLogin } from "./redux/actions";
 import { userSelector } from "./redux/selectors";
 function App() {
-  const dispatch = useDispatch()
-  const user = useSelector(userSelector)
+  const dispatch = useDispatch();
+  const user = useSelector(userSelector);
   useEffect(() => {
     const checkToken = async () => {
       try {
         const token = localStorage.getItem("token");
         if (token && token !== "undefined") {
-          const getUserToken = getUserByToken(); 
+          const getUserToken = getUserByToken();
           getUserToken
             .then((res) => {
               console.log(res.data.data);
               res.data.data = Object.assign(res.data.data, { token });
-                dispatch(addUserLogin(res.data.data));
-                dispatch(setDataLogin(res.data.data));
-              
+              dispatch(addUserLogin(res.data.data));
+              dispatch(setDataLogin(res.data.data));
             })
             .catch((res) => {
-              localStorage.removeItem("token");
+              // localStorage.removeItem("token");
               return <Navigate to={"/login"} replace={true}></Navigate>;
             });
-        }else{
+        } else {
           return <Navigate to={"/login"} replace={true}></Navigate>;
         }
       } catch (error) {
-        localStorage.removeItem("token");
+        //localStorage.removeItem("token");
         return <Navigate to={"/login"} replace={true} />;
       }
     };
