78b4d41481fe578de2d613d559134dd1a4de74b3
Duong Minh Tan <dmtan1510@gmail.com>
2023-07-27 11:40:08 +0700

    tan

--- frontend/src/pages/components/NotificationNew/NewsList.jsx
+++ frontend/src/pages/components/NotificationNew/NewsList.jsx
@@ -5,6 +5,8 @@ import { Typography } from '@mui/material';
 import Footer from '../Footer/Footer';
 import Navigation from '../Header/Navigation/Navigation';
 import { CSSTransition } from 'react-transition-group';
+
+const ITEMS_PER_PAGE = 4; // Number of news items per page
 const baseUrl = 'https://yoga-app-swp.onrender.com/api/news/get';
 
 function NewsList() {
@@ -12,35 +14,37 @@ function NewsList() {
   const [listOrigin, setListOrigin] = useState([]);
   const [selectedNews, setSelectedNews] = useState(null);
   const [isNewsSelected, setIsNewsSelected] = useState(false);
- 
-  useEffect(() => {
-    const test = async () => {
-      const { data } = await axios.get(`${baseUrl}`);
-      data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-      const filterData = data.filter((item) => item.status === true);
-      const latestItem = filterData[0];
-      setListOrigin([latestItem]);
-    };
-    test();
-  }, []);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    function compare(a, b) {
-      if (a.createdAt < b.createdAt) {
-        return 1;
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(baseUrl);
+        data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        const filterData = data.filter((item) => item.status === true);
+        setListOrigin([filterData[0]]);
+        setList(filterData);
+      } catch (error) {
+        console.error('Error fetching news:', error);
       }
-      if (a.createdAt > b.createdAt) {
-        return -1;
-      }
-      return 0;
-    }
-    const test = async () => {
-      const { data } = await axios.get(`${baseUrl}`);
-      const filterData = data.filter((item) => item.status === true);
-      setList(filterData.sort(compare));
     };
-    test();
+    fetchData();
   }, []);
+
+  // Calculate the total number of pages based on the number of news items
+  const totalPages = Math.ceil(list.length / ITEMS_PER_PAGE);
+
+  // Function to handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Function to get the current page's news items
+  const getCurrentPageItems = () => {
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    return list.slice(startIndex, endIndex);
+  };
 
   const handleNewsClick = (news) => {
     setSelectedNews(news);
@@ -65,11 +69,10 @@ function NewsList() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          marginTop:"20px"
-          ,marginBottom:"10px"
+          marginTop: "20px",
+          marginBottom: "10px",
         }}
       >
-        {' '}
         <Typography variant="h4" gutterTop style={{ fontFamily: 'SangBleu Sunrise' }}>
           NOTIFICATIONS
         </Typography>
@@ -86,7 +89,7 @@ function NewsList() {
         }}
       >
         {isNewsSelected && selectedNews ? (
-          <div style={{ flex: '1', maxWidth: '60rem' ,marginLeft:"20px" }}>
+          <div style={{ flex: '1', maxWidth: '60rem', marginLeft: "20px" }}>
             <div className="card px-3 pt-3">
               <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <div className="bg-image hover-overlay shadow-1-strong ripple rounded-5 mb-4" data-mdb-ripple-color="light">
@@ -113,35 +116,42 @@ function NewsList() {
           </div>
         ) : null}
 
-        <div style={{ flex: '1', marginLeft:"50px" }}>
-          {list.map((news, index) => {
-            return (
-              
-              <div key={index} onClick={() => handleNewsClick(news)}>
-                <div className="row mb-4 border-bottom pb-2">
-                  <div className="col-3" style={{ marginTop: '10px' }}>
-                    <img
-                      src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
-                      className="img-fluid shadow-1-strong rounded"
-                      alt="Hollywood Sign on The Hill"
-                    />
-                  </div>
-
-                  <div className="col-9">
-                    <strong>{news.subject}</strong>
-                    <p>
-                      <u>{moment(news.createdAt).format('DD/MM/YY')}</u>
-                    </p>
-                    <div>{news.content}</div>
-                  </div>
+        <div style={{ flex: '1', marginLeft: "50px" }}>
+          {getCurrentPageItems().map((news, index) => (
+            <div key={index} onClick={() => handleNewsClick(news)} style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', cursor: 'pointer' }}>
+              <div className="row mb-4 border-bottom pb-2">
+                <div className="col-3" style={{ marginTop: '10px' }}>
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp"
+                    className="img-fluid shadow-1-strong rounded"
+                    alt="Hollywood Sign on The Hill"
+                  />
                 </div>
-                
+
+                <div className="col-9">
+                  <strong>{news.subject}</strong>
+                  <p>
+                    <u>{moment(news.createdAt).format('DD/MM/YY')}</u>
+                  </p>
+                  <div>{news.content}</div>
+                </div>
               </div>
-             
-            );
-          })}
+            </div>
+          ))}
         </div>
       </div>
+
+      {/* Pagination controls */}
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+        <button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+          Previous Page
+        </button>
+        <span style={{ margin: '0 10px' }}>Page {currentPage} of {totalPages}</span>
+        <button disabled={currentPage === totalPages} onClick={() => handlePageChange(currentPage + 1)}>
+          Next Page
+        </button>
+      </div>
+
       <Footer />
     </div>
   );
